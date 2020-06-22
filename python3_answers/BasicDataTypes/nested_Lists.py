a4705dd39743d73489dd64bba39c586214b1b411
livetoworldlife <55538102+livetoworldlife@users.noreply.github.com>
2020-06-23 00:44:00 +0200

    Update nested_Lists.py

--- python3_answers/BasicDataTypes/nested_Lists.py
+++ python3_answers/BasicDataTypes/nested_Lists.py
@@ -16,3 +16,17 @@ for nm,sc in liste:
     if sc == second:
         print(nm)
         
+
+# second solution        
+if __name__ == '__main__':
+    a = int(input())
+    marksheet = []
+    
+    for _ in range(a):
+        name = input()
+        score = float(input())
+        marksheet.append([name, score])
+        
+
+second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
